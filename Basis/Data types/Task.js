2fc618868ef74b9326e3ea064be9c3fb6cc5cad8
NadinFomina <8241021nf@mail.ru>
2023-02-26 11:49:35 +0300

    изучение JS

---
+++ Basis/Data types/Task.js
@@ -0,0 +1,8 @@
+let name = "Ilya";
+
+alert( `hello ${1}` ); //  hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
+// используя обратные ккавычки, мы можем писать сложные выражения используя переменные
